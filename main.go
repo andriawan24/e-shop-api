f2b9749f24a2956e2a6a5a955e593329201c6047
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2022-05-31 00:00:58 +0700

    Add project structure

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	api := router.Group("/api/v1")
+
+	api.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping success",
+		})
+	})
+
+	router.Run()
 }
